b60e17bb521fbfb085e556fd1c959fa30d64e2cb
Raihan-brain <raihanuzzaman@bs-23.net>
2020-03-15 15:21:05 +0600

    added navigation and form submit

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../models/recipe.model';
 
 @Component({
@@ -8,10 +8,15 @@ import { Recipe } from '../../models/recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelected() {
+    console.log('in 1 =======================');
+    this.recipeSelected.emit();
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../models/recipe.model';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Recipe } from "../models/recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'A test recipt',
-      'This is simply a test',
-      'https://www.cscassets.com/recipes/wide_cknew/wide_50780.jpg'
+      "A test recipt",
+      "This is simply a test",
+      "https://www.cscassets.com/recipes/wide_cknew/wide_50780.jpg"
     ),
     new Recipe(
-      'A test recipt',
-      'This is simply a test',
-      'https://www.cscassets.com/recipes/wide_cknew/wide_50780.jpg'
+      "A test recipt",
+      "This is simply a test 223232",
+      "https://www.cscassets.com/recipes/wide_cknew/wide_50780.jpg"
     )
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipeEl: Recipe) {
+    this.recipeWasSelected.emit(recipeEl);
+  }
 }
