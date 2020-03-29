69eb4c7fb5d1fdfa115638b580a0d74211a00ae7
Raihan-brain <riger400@gmail.com>
2020-03-29 12:13:51 +0600

    added more routes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../models/recipe.model";
 import { RecipeServiceService } from "../service/recipe-service.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-recipe-list",
@@ -10,9 +11,17 @@ import { RecipeServiceService } from "../service/recipe-service.service";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeServiceService) {}
+  constructor(
+    private recipeService: RecipeServiceService,
+    private route: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipe();
+  }
+
+  openNewRecipe() {
+    this.route.navigate(["new"], { relativeTo: this.activatedRoute });
   }
 }
